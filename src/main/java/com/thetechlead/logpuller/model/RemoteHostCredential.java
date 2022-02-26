82ee23a57075c48b723eb17916d96889ab3a85e1
nguyenduyy <duyy.uit@gmail.com>
2022-02-26 22:17:49 +0700

    download via scp

--- src/main/java/com/thetechlead/logpuller/model/RemoteHostCredential.java
+++ src/main/java/com/thetechlead/logpuller/model/RemoteHostCredential.java
@@ -32,4 +32,6 @@ public class RemoteHostCredential {
     String remoteFolderPath;
 
     String mainLogFileName;
+
+    String downloadLogLocalPath;
 }
--- src/main/java/com/thetechlead/logpuller/service/ScpService.java
+++ src/main/java/com/thetechlead/logpuller/service/ScpService.java
@@ -6,11 +6,14 @@ import com.thetechlead.logpuller.model.RemoteHostCredential;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.io.File;
 import java.util.Vector;
 
 public class ScpService {
 
     public static ScpService INSTANCE = null;
+
+    public static final String PATHSEPARATOR = "/";
 
     public static ScpService getInstance() {
         if (INSTANCE == null) {
@@ -33,9 +36,10 @@ public class ScpService {
         ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
 
         String remoteFolder = credential.getRemoteFolderPath();
+
         try {
             channelSftp.connect();
-            Vector result = channelSftp.ls(remoteFolder);
+            recursiveFolderDownload(channelSftp, remoteFolder, credential.getDownloadLogLocalPath());
         } catch (SftpException e) {
             e.printStackTrace();
 
@@ -44,6 +48,30 @@ public class ScpService {
             session.disconnect();
         }
 
+    }
+
+    private void recursiveFolderDownload(ChannelSftp channelSftp, String sourcePath, String destinationPath) throws SftpException {
+        Vector<ChannelSftp.LsEntry> fileAndFolderList = channelSftp.ls(sourcePath); // Let list of folder content
+        // Iterate through list of folder content
+        for (ChannelSftp.LsEntry item : fileAndFolderList) {
+            if (!item.getAttrs().isDir()) { // Check if it is a file (not a directory).
+                if (!(new File(destinationPath + PATHSEPARATOR + item.getFilename())).exists()
+                        || (item.getAttrs().getMTime() > Long
+                        .valueOf(new File(destinationPath + PATHSEPARATOR + item.getFilename()).lastModified()
+                                / (long) 1000)
+                        .intValue())) { // Download only if changed later.
+                    new File(destinationPath + PATHSEPARATOR + item.getFilename());
+                    // Download file from source (sourcefilename, destination filename).
+                    channelSftp.get(sourcePath + PATHSEPARATOR + item.getFilename(),
+                            destinationPath + PATHSEPARATOR + item.getFilename());
+                }
+            } else if (!(".".equals(item.getFilename()) || "..".equals(item.getFilename()))) {
+                new File(destinationPath + PATHSEPARATOR + item.getFilename()).mkdirs(); // Empty folder copy.
+                // Enter found folder on server to read its contents and create locally.
+                recursiveFolderDownload(channelSftp, sourcePath + PATHSEPARATOR + item.getFilename(),
+                        destinationPath + PATHSEPARATOR + item.getFilename());
+            }
+        }
     }
 
     // on local machine: ssh-keygen -t rsa -b 4096 -m PEM  -C "logpuller" -f ./id_rsa
@@ -58,6 +86,7 @@ public class ScpService {
                 .publicKeyFilePath("/Users/nguyenduyy/Workspaces/LogPuller/testdata/id_rsa.pub")
                 .privateKeyPassword("pppppp")
                 .remoteFolderPath("/home/duyy.uit/guestapp/server/var/log/guestapp")
+                .downloadLogLocalPath("./data/guestapp")
                 .build();
         try {
             scpService.downloadFolders(remoteHostCredential);
